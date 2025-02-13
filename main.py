6549bf497ef6814e8eb6819026f665822d7b7a6e
Maris214 <dmrodriguez1021@gmail.com>
2025-02-13 15:28:09 -0500

    Created python file for constant variables and imported it into the main file

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pygame
+from constants import *
 
 def main():
-    print("Starting asteroids!")
+    print(f"""
+    Starting asteroids!
+    Screen width: {SCREEN_WIDTH}
+    Screen height: {SCREEN_HEIGHT} 
+    """)
 
 
 if __name__ == "__main__":
