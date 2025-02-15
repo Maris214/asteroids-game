6ec9fba6fc883897233a145f3030a5e1a9ebb245
Maris214 <dmrodriguez1021@gmail.com>
2025-02-15 09:30:13 -0500

    player movement enabled

--- game/main.py
+++ game/main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
 
 def main():
     pygame.init()
@@ -9,15 +9,24 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+
+        for item in drawable:
+            item.draw(screen)
+            
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60) / 1000
